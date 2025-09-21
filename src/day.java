8d0c52a10d69c1c4fd5ac2256cbc74bc24eea3a8
chanchalsingh2003 <chanchalsingh21003@gmail.com>
2025-09-21 20:29:24 +0530

    new

--- src/day.java
+++ src/day.java
@@ -1,5 +1,6 @@
 public class day {
     public static void main(String args[]){
-        System.out.println("sghsgh");lllkkkkk
+        System.out.println("sghsgh");
+       // ho gya sayd
     }
 }
