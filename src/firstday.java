4ffbea1864c81abb5f41dc57c2fb8a8121e7c987
chanchalsingh2003 <chanchalsingh21003@gmail.com>
2025-09-21 19:06:17 +0530

    change

---
+++ src/firstday.java
@@ -0,0 +1,76 @@
+public class firstday {
+    public static void main(String args[]) {
+
+// local variable
+       /* int num = 10;
+        System.out.println(" Variable: " + num);
+
+        //add two values
+        int a=10;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);*/
+
+        //convert int to float
+          /*int a=10;
+        float f=a;
+        System.out.println(a);
+        System.out.println(f);
+
+
+        //typecasting
+        float f=10.5f;
+        int a=(int)f;
+        System.out.println(f);
+        System.out.println(a);
+
+
+        //overflow
+        int a=130;
+        byte b=(byte)a;
+        System.out.println(a);
+        System.out.println(b);
+
+        // addinjg lower type
+        byte a=10;
+        byte b=10;
+        byte c=(byte)(a+b);
+        System.out.println(c);
+
+
+        // data types
+
+        boolean flag = true;
+        char ch = 'z';
+        int num = 1234;
+        byte size = 2;
+        short srt = 78;
+        double value = 2.4546778;
+        float temp = 3.8f;
+        long val = 1888889;
+        System.out.println("boolean: " + flag);
+        System.out.println("char: " + ch);
+        System.out.println("integer: " + num);
+        System.out.println("byte: " + size);
+        System.out.println("short: " + srt);
+        System.out.println("float: " + value);
+        System.out.println("double: " + temp);
+        System.out.println("long: " + val);
+
+
+
+        // print string and array
+
+        String str = "Hello";
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(str);
+        for (int member : arr) {
+            System.out.print(member + ", ");*/
+        }
+    }
+
+
+
+
+
+
